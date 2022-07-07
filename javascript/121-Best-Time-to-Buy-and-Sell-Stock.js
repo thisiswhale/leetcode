aabf8a9d32b174bb43cf5f6d2d15557d8db1509f
William Leung <leungwk25@gmail.com>
2022-07-06 22:25:47 -0700

    Improve readability
    Update 121-Best-Time-to-Buy-and-Sell-Stock.js file for better readability

--- javascript/121-Best-Time-to-Buy-and-Sell-Stock.js
+++ javascript/121-Best-Time-to-Buy-and-Sell-Stock.js
@@ -3,14 +3,13 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let buy = prices[0];
-    let profit = 0; 
-    for (let i = 0; i < prices.length; i++) {
-        if (prices[i] < buy) {
-            buy = prices[i];
-        } else {
-            profit = Math.max(prices[i] - buy, profit);
-        }
+    let l =0,r=1;
+    let max = 0;
+    
+    while(r < prices.length){
+        if(prices[l] < prices[r]) max = Math.max(max, prices[r] - prices[l])
+        else l = r;
+        r++
     }
-    return profit;
+    return max;
 };
